57a3a09eeb7de64e3652cb921ea1e48164a634fe
Jitendra Maharana <deepakmaharana3500@gmail.com>
2025-09-16 12:31:43 +0530

    project complete

--- Portfolio/src/components/Footer/Footer.jsx
+++ Portfolio/src/components/Footer/Footer.jsx
@@ -57,7 +57,7 @@ const Footer = () => {
 
         {/* Copyright Text */}
         <p className="text-sm text-gray-400 mt-6">
-          © 2025 Deepak Maharana. All rights reserved.
+          © 2025 Deepak Maharana.  All rights reserved.
         </p>
       </div>
     </footer>
